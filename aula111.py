b7ba8a0016e121b55b82be1fcb5eda0c89aedac2
Bianca Luiz <techy.bluiz@gmail.com>
2024-04-26 12:09:55 -0300

    Update aula111.py

--- aula111.py
+++ aula111.py
@@ -1,7 +1,5 @@
 #map - para mapear dados
-
 from functools import partial
-
 produtcs = [
     {'produtcs 1' : 'milk','price': 10.00},
     {'produtcs 2' : 'sugar','price': 5.00},
@@ -9,10 +7,8 @@ produtcs = [
     {'produtcs 4' : 'rice','price': 20.00},
     {'produtcs 5' : 'beans','price': 9.00},
 ]
-
 def increase_price(vallue, percentage):
     return round(vallue * percentage, 2)
-
 increase_ten_percentage = 'teste'
 
 new_produtcs = [
